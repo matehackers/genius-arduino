546a5c47a7d7b5dabf37d3fed7610c94b221ee54
Lucas Zawacki <lfzawacki@gmail.com>
2013-03-31 03:11:05 -0300

    Generalizado para 'n' botoes e com som

--- genius.ino
+++ genius.ino
@@ -2,13 +2,13 @@
 #define SEQUENCE_MAX 32
 #define END_SEQUENCE -1
 
-int buttonInputs[] = { 2, 12 };
-int nInputs = 2;
+int buttonInputs[] = { 2, 3, 4, 5 };
+int nInputs = 4;
 
-int ledOutputs[] = { 5, 10 };
-int nOutputs = 2;
+int ledOutputs[] = { 9, 10, 11, 12 };
+int nOutputs = 4;
 
-int beepOutput = 9;
+int beepOutput = 6;
 
 int sequence[SEQUENCE_MAX];
 
@@ -47,15 +47,78 @@ void initSequence()
   }
 }
 
+// Retorna o indice do botao apertado ou -1 se nenhum foi apertado
+int checkButtons()
+{
+   int button = -1;
+
+   // Testar todos botoes
+   for (int i=0; i < nInputs && button == -1; i++)
+   {
+     if (digitalRead(buttonInputs[i]) == LOW)
+     {
+       button = i;
+     }
+   }
+
+   return button;
+}
+
+int noteToFreq(int note)
+{
+  return (int) pow(2, (double)note/12) * 440;
+}
+
+// Toca uma melodia num alto falate ou buzzer
+void playMelody(int *mel, int *dur, int n)
+{
+  for(int i=0; i < n; i++)
+  {
+    tone(beepOutput, noteToFreq(mel[i]), dur[i]*100);
+    delay(dur[i]*100);
+    noTone(beepOutput);
+  }
+}
+
+void playStart()
+{
+  int n = 7;
+  int mel[] = {30, 34, 37, 42, 37, 34, 30};
+  int dur[] = {1, 1, 1, 2, 1, 1, 4};
+  playMelody(mel, dur, n);
+}
+
+void playEnd()
+{
+  int n = 12;
+  int mel[] = {42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31};
+  int dur[] = { 1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  8};
+  playMelody(mel, dur, n);
+}
+
 // Função para rapidamente piscar os LEDs
 void blinkLeds(int t)
 {
-    digitalWrite(ledOutputs[0], HIGH);
-    digitalWrite(ledOutputs[1], HIGH);
+    for (int i=0; i < nInputs; i++)
+    {
+      digitalWrite(ledOutputs[i], HIGH);
+    }
     delay(t/2);
-    digitalWrite(ledOutputs[0], LOW);
-    digitalWrite(ledOutputs[1], LOW);
+    for (int i=0; i < nInputs; i++)
+    {
+      digitalWrite(ledOutputs[i], LOW);
+    }
     delay(t/2);
+}
+
+// Faz um beep com a frequencia do botao apertado
+void beep(int button)
+{
+  int freqs[] = {30, 32, 34, 37};
+  int freq = noteToFreq(freqs[button]);
+  tone(beepOutput, freq, 1000);
+  delay(1000);
+  noTone(beepOutput);
 }
 
 void loop()
@@ -64,8 +127,7 @@ void loop()
   delay(1000);
 
   Serial.println("Esperando aperto para iniciar");
-  while (digitalRead(buttonInputs[0]) == HIGH &&
-        digitalRead(buttonInputs[1]) == HIGH)
+  while (checkButtons() == -1)
   {
     blinkLeds(300);
   }
@@ -75,13 +137,16 @@ void loop()
 
   // Cria uma nova sequencia
   initSequence();
+  // Melodia do inicio
+  playStart();
 
   // Temos certeza do inicio do jogo aqui
   while (correct)
-  {    
+  {
     delay(500);
-    digitalWrite(ledOutputs[0], LOW);
-    digitalWrite(ledOutputs[1], LOW);
+    // Reseta todos os botoes
+    for (int i=0; i < nOutputs; i++)
+      digitalWrite(ledOutputs[i], LOW);
     delay(500);
 
     // Toca uma sequencia
@@ -91,51 +156,49 @@ void loop()
       int led = sequence[pos];
 
       digitalWrite(ledOutputs[led], HIGH);
-      delay(1000);
-      digitalWrite(ledOutputs[led], LOW); 
-      delay(1000);
 
       pos += 1;
       Serial.print("Toca: ");
       Serial.println(led);
+      beep(led);
+      digitalWrite(ledOutputs[led], LOW);
+      delay(1000);
     }
-    
+
     int nPressed = 0;
-    
+
+    delay(1000);
+
     // Fazendo a leitura da resposta
     while (nPressed < pos && correct)
     {
-      delay(1000);
-      digitalWrite(ledOutputs[0], LOW);
-      digitalWrite(ledOutputs[1], LOW);
+      for (int i=0; i < nOutputs; i++)
+        digitalWrite(ledOutputs[i], LOW);
 
-       while (digitalRead(buttonInputs[0]) == HIGH &&
-              digitalRead(buttonInputs[1]) == HIGH)
+      int button = checkButtons();
+
+      while (button == -1)
       {
           delay(200);
+          button = checkButtons();
       }
-  
-      // Primeiro botão pressionado e correto
-      if (digitalRead(buttonInputs[0]) == LOW && sequence[nPressed] == 0)
+
+      if (button == sequence[nPressed])
       {
-        digitalWrite(ledOutputs[0], HIGH);
+        digitalWrite(ledOutputs[button], HIGH);
         Serial.println("Correto");
-        correct = true;
-      }
-      // Segundo botão pressionado e correto
-      else if (digitalRead(buttonInputs[1]) == LOW && sequence[nPressed] == 1)
-      {
-        digitalWrite(ledOutputs[1], HIGH);
-        Serial.println("Correto 2");
+        beep(button);
         correct = true;
       }
       else
       {
-        digitalWrite(ledOutputs[0], HIGH);
-        digitalWrite(ledOutputs[1], HIGH);
+        for (int i=0; i < nOutputs; i++)
+          digitalWrite(ledOutputs[i], HIGH);
 
         Serial.println("Errado");
         correct = false;
+        // Melodia do fim
+        playEnd();
       }
 
       nPressed += 1;
